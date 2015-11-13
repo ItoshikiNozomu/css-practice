a0fcb85b5fe13999748435e877b61ec7ea78fe7c
unknown <luhuan@eccyf-luhuan1.ecc.360buyAD.local>
2015-11-13 16:54:19 +0800

    gu

--- src/algorithms/trees.js
+++ src/algorithms/trees.js
@@ -41,10 +41,10 @@ class BstNode{
 	
 	
 	constructor(value){
-		this.val = value
+		this.value = value
 		this.left = null
 		this.right = null
-	}
+	} 
 }
 
 class BinarySearchTree{
@@ -58,30 +58,36 @@ class BinarySearchTree{
 		}
 	}
 	insert(value) {
+		var _this = this
 		var insertNode = function(target,newNode){
-			if(target.value>newNode.value){
+			if(target.value>=newNode.value){
 				if(target.left){
-					
+					insertNode(target.left,newNode)
 				}else{
+					target.left = newNode
 					
 				}
-			}else if(target.value===newNode.value){
-				
 			}else{
-				
+				if(target.right){
+					insertNode(target.right,newNode)
+				}else{
+					target.right = newNode
+				}
 			}
 		}
 		var node = new BstNode(value)
-		if (!this.root) {
-			this.root = node
+		if (!_this.root) {
+			_this.root = node
 		} else {
-			insertNode(this.root,node)
+			insertNode(_this.root,node)
 		}
 
 
 	}
 	search(value){}
-	inOrderTraverse(){}
+	inOrderTraverse(cb){
+		
+	}
 	preOrderTraverse(){}
 	postOrderTraverse(){}
 	min(){}
@@ -90,3 +96,126 @@ class BinarySearchTree{
 	
 }
   
+
+var mBst = new BinarySearchTree(10)
+for(var i = 5;i<15;i++){
+	mBst.insert(i)
+}
+console.log(mBst)
+
+
+class Graph{
+	
+	constructor(){
+		this.vertices = []
+		this.neighbours = {}
+		
+	}
+	addVertex(v){
+		this.vertices.push(v)
+		this.neighbours[v] = []
+	}
+	addEdge(vertex,neighbour){
+		//this.vertices.push(vertex)
+		this.neighbours[vertex].push(neighbour)
+		this.neighbours[neighbour].push(vertex)
+	}
+	
+	toString(){
+		var str = ''
+		for(let v of this.vertices){
+			let nList = this.neighbours[v]
+			str+=v+'->'+nList.join(',')+'\n'
+		}
+		return str
+	}
+	
+	bfs(vertex,cb){
+		const WHITE = 'white'
+		const GREY = 'grey'
+		const BLACK = 'black'
+		let mQueue = []
+		let colors = {}
+		let distances = {}
+		//前溯点
+		let preds = {}
+		for(let v of this.vertices){
+			colors[v] = WHITE
+			distances[v ]= 0
+			preds[v] = null
+		}
+		
+		
+		mQueue.push(vertex)
+		
+		while(mQueue.length>0){
+			console.log(mQueue)
+			//observer
+			let mVertex = mQueue.shift()
+			let neighbours = this.neighbours[mVertex]
+			
+			colors[mVertex] = GREY
+		
+			//explore
+			
+			for(let v of neighbours){
+				if(colors[v]==WHITE&&!mQueue.some((ele)=>{return ele==v})){
+					mQueue.push(v)
+					distances[v] = distances[mVertex]+1
+					preds[v] = mVertex
+				}
+				
+			}
+			colors[mVertex] = BLACK
+			if(cb)
+				cb(mVertex)
+		}
+		return {
+			distances:distances,
+			preds:preds
+		}
+	}
+	
+	dfs(){}
+	
+	getShortestPath(fr,to){
+		let pathInfo = this.bfs(fr)
+		let path = []
+		let mVert = to
+		while(mVert!=null){
+			path.push(mVert)
+			mVert = pathInfo.preds[mVert]
+		}
+		return path.reverse()
+		
+	}
+}
+
+let mGraph = new Graph()
+
+var mVerts = ['a','b','c','d','e','f','g','h','i']
+
+
+for(let v of mVerts){
+	mGraph.addVertex(v)
+}
+mGraph.addEdge('a','b')
+mGraph.addEdge('a','c')
+mGraph.addEdge('a','d')
+mGraph.addEdge('c','d')
+mGraph.addEdge('c','g')
+mGraph.addEdge('d','g')
+mGraph.addEdge('d','h')
+mGraph.addEdge('b','e')
+mGraph.addEdge('b','f')
+mGraph.addEdge('e','i')
+
+
+
+console.log(mGraph.toString())
+
+console.log(mGraph.bfs('a',(v)=>{console.log(v+',')}))
+
+
+console.log(mGraph.getShortestPath('a','i'))
+
